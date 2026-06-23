f7559d9b0f1cf1bced88f0156da4e32c492758fe
fandhi19 <ilyasfandhianggara@gmail.com>
2026-06-23 15:09:09 +0700

    up2

--- resources/views/tracking.blade.php
+++ resources/views/tracking.blade.php
@@ -132,21 +132,21 @@
                     </div>
 
                     {{-- Notifikasi Status --}}
-                    @if($peminjaman->status == 'disetujui')
+                    @if($peminjaman->status == 'Disetujui')
                         <div class="mt-6 bg-green-50 border border-green-200 text-green-800 rounded-lg p-4">
                             <i class="fa-solid fa-circle-check mr-2"></i> Peminjaman Anda telah <strong>disetujui</strong>. Silakan gunakan ruangan sesuai jadwal.
                         </div>
-                    @elseif($peminjaman->status == 'ditolak')
+                    @elseif($peminjaman->status == 'Ditolak')
                         <div class="mt-6 bg-red-50 border border-red-200 text-red-800 rounded-lg p-4">
                             <i class="fa-solid fa-circle-xmark mr-2"></i> Maaf, pengajuan Anda <strong>ditolak</strong>. Hubungi admin untuk informasi lebih lanjut.
                         </div>
-                    @elseif($peminjaman->status == 'selesai')
+                    @elseif($peminjaman->status == 'Selesai')
                         <div class="mt-6 bg-gray-50 border border-gray-200 text-gray-800 rounded-lg p-4">
                             <i class="fa-solid fa-flag-checkered mr-2"></i> Peminjaman ini telah <strong>selesai</strong>.
                         </div>
                     @else
                         <div class="mt-6 bg-yellow-50 border border-yellow-200 text-yellow-800 rounded-lg p-4">
-                            <i class="fa-solid fa-hourglass-half mr-2"></i> Pengajuan Anda masih <strong>pending</strong>. Admin akan segera meninjaunya.
+                            <i class="fa-solid fa-hourglass-half mr-2"></i> Pengajuan Anda masih <strong>Pending</strong>. Admin akan segera meninjaunya.
                         </div>
                     @endif
                 </div>
